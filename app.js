01149194e2505cd574b954be760c22bfde51dd54
kaluaim <kaluaim@gmail.com>
2017-03-25 23:21:53 -0400

    added basic ui views

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const static = express.static(__dirname + '/public');
+
+let app = express();
+
+app.use("/public", static);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/ui/index.html');
+})
+
+app.get('/need/', function (req, res) {
+    res.sendFile(__dirname + '/ui/need/list.html');
+})
+
+app.get('/need/1', function (req, res) {
+    res.sendFile(__dirname + '/ui/need/item.html');
+})
+
+app.get('/share/', function (req, res) {
+    res.sendFile(__dirname + '/ui/share/share-item.html');
+})
+
+app.post('/share', function (req, res) {
+    console.log(req);
+})
+
+app.listen(3000, () => {
+    console.log("Your routes will be running on http://localhost:3000");
+});

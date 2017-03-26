c78e07536fa49c5eb7cac0477fa8d5c88bfb16d2
Paras Garg <pgarg2@stevens.edu>
2017-03-25 22:02:06 -0400

    MongoDB Connection Configuration

---
+++ config/mongodbCollection.js
@@ -0,0 +1,26 @@
+const dbConnection = require('./mongodbConnection');
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+let getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return () => {
+        if (!_col) {
+
+            _col = dbConnection().then((db) => {
+                return db.collection(collection);
+            });
+        }
+
+        return _col;
+    }
+}
+
+/* Now, you can list your collections here: */
+module.exports = {
+    users: getCollectionFn("users"),
+    credentials: getCollectionFn("credentials"),
+    sharesList: getCollectionFn("sharesList"),
+    sharesHistory: getCollectionFn("sharesHistory")
+};
